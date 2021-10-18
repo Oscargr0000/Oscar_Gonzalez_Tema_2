5524e339a4296ec46d7d353c94f6684ac8aa669d
Oscargr0000 <oscargr0000@gmail.com>
2021-10-18 10:47:27 +0200

    18/10/2021

--- Tema 2/Assets/scipts/MyFirstScript.cs
+++ Tema 2/Assets/scipts/MyFirstScript.cs
@@ -4,10 +4,66 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+    public int x = 5;
+    public int y = 2;
+
+    public float a = 5f;
+    public float b = 2f;
+
+    public bool Isranning = false;
+
+    public int PlayerAge = 18;
+
+
+
+   [SerializeField] private bool HasSiren = true;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        /*
+        Debug.Log($"Suma: {x} + {y} = {x + y}");
+        Debug.Log($"Resta: {x} - {y} = {x - y}");
+        Debug.Log($"Division: {x} / {y} = {x / y}");
+        Debug.Log($"multiplicacion: {x} * {y} = {x * y}");
+
+        Debug.Log(string.Format("Product: {0} / {1} = {2}", x, y, x / y));
+        */
+
+        if (Isranning == true)
+        {
+            Debug.Log("Lleva paraguas."); 
+
+        } else
+        {
+            Debug.Log("No llueve, no lleves paraguas");
+        }
+
+        //Indicador de posesion de sirena.
+        if (HasSiren)
+        {
+
+            Debug.Log($"{name} tiene sirena");
+        } else
+        {
+            Debug.Log($"{name} no tiene sirena");
+        }
+
+
+        if (PlayerAge >= 18)
+        {
+            Debug.Log($"Eres mayor de edad");
+        } else if (PlayerAge >= 13)
+        {
+            Debug.Log($"Eres un adolescente");
+        } else
+        {
+            Debug.Log($"Eres un niño");
+        }
+
     }
 
     // Update is called once per frame
--- Tema 2/Assets/scipts/Vehicle.cs
+++ Tema 2/Assets/scipts/Vehicle.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
+    /*
     public int playerAge = 35;
     private float playerSpeed = 5.25f;
     public string playerName = "Oscar";
-    [SerializeField] private bool gameOver = false;
-    
+    [SerializeField] private bool gameOver = false;ç
+
+
+    //Variable que guarda el sonido del vehiculo
+
+    */
+
+
+    [SerializeField] private string name;
+    [SerializeField] private int NumberWheels = 4;
+    [SerializeField] private string Sound = "Brum, Brum";
+    [SerializeField] private bool isOn = true;
+    [SerializeField] private bool HasSiren = true;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       /*
+        Debug.Log($"{name} tiene {NumberWheels} ruedas y hace {Sound}");
+       */
+
+        if (isOn == true)
+        {
+            Debug.Log($"{Sound}");
+        } else if (HasSiren == true)
+        {
+            Debug.Log($"{name} hara {Sound} cuando se ponga en marcha");
+        } else
+        {
+            Debug.Log($"{name} esta en marcha");
+        }
     }
 
     // Update is called once per frame
